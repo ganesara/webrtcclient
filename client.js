0c60d8265cf8b7a9a869663b74310bfff2b0769f
Ramanathan <ramanathang.ganean@gmail.com>
2022-04-24 20:51:50 +0800

    Connecting to signaling server

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-var connection = new WebSocket('ws://localhost:9090'); 
+var connection = new WebSocket('ws://webrtcserver.azurewebsites.net:443'); 
 var name = "";
 
 var loginInput = document.querySelector('#loginInput'); 
